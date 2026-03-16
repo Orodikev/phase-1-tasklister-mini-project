1c279cb32dd93c6dad0411e63bf5007690aa9832
Orodikev <kevinyengo@gmail.com>
2026-03-16 05:07:39 +0300

    Tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('#create-task-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const input = document.querySelector('#new-task-description')
+    const taskList = document.querySelector('#tasks')
+    const newTask = document.createElement('li')
+    newTask.textContent = input.value
+    taskList.appendChild(newTask)
+    input.value = ''
+  }
   // your code here
-});
+,)})
